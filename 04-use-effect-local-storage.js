1b320e1bc508b268c2c11577b262b4da8c98f672
David Dinh <dmdinh22@gmail.com>
2018-11-04 21:26:59 -0800

    clean up formatting

--- 04-use-effect-local-storage.js
+++ 04-use-effect-local-storage.js
@@ -4,13 +4,16 @@ function Counter() {
     const [count, setCount] = useState(() =>
         Number(window.localStorage.getItem('count') || 0)
     );
+
     const increment = () => setCount(count + 1);
+
     useEffect(
         () => {
             window.localStorage.setItem('count', count);
         },
         [count]
     );
+
     return <button onClick={increment}>{count}</button>;
 }
 
