2f9c93469b5c79f5f3cb7eb3548452cc87ea723e
David Dinh <dmdinh22@gmail.com>
2018-11-06 00:00:50 -0800

    stopwatch with state

--- 06-vanilla-tilt.js
+++ 06-vanilla-tilt.js
@@ -4,6 +4,7 @@ import VanillaTilt from 'vanilla-tilt';
 
 function Tilt(props) {
     const tiltRef = useRef();
+
     useEffect(() => {
         VanillaTilt.init(tiltRef.current, {
             max: 25,
@@ -11,8 +12,10 @@ function Tilt(props) {
             glare: true,
             'max-glare': 0.5
         });
+
         return () => tiltRef.current.vanillaTilt.destroy();
     }, []);
+
     return (
         <div ref={tiltRef} className="tilt-root">
             <div className="tilt-child">{props.children}</div>
